d919b983574a5dea520f55c2a94c0e2ef847bd28
Diego Macario <diego_macario@hotmail.com>
2026-05-18 19:53:41 -0300

    Client - fix: substitui GrpcChannelFactory por ManagedChannelBuilder direto
    O GrpcChannelFactory do Spring gRPC resultava em "Connection refused"
    ao tentar conectar no servidor. Substituido por ManagedChannelBuilder
    com usePlaintext() e endereco explicito 127.0.0.1:8080.

--- grpc-client/src/main/java/com/grpc/client/infrastructure/config/GrpcClientConfig.java
+++ grpc-client/src/main/java/com/grpc/client/infrastructure/config/GrpcClientConfig.java
@@ -2,16 +2,21 @@ package com.grpc.client.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.grpc.client.GrpcChannelFactory;
 
 import com.grpc.client.BookServiceGrpc;
 import com.grpc.client.BookServiceGrpc.BookServiceBlockingStub;
+
+import io.grpc.ManagedChannelBuilder;
 
 @Configuration
 public class GrpcClientConfig {
 
     @Bean
-    public BookServiceBlockingStub bookServiceBlockingStub(GrpcChannelFactory factory) {
-        return BookServiceGrpc.newBlockingStub(factory.createChannel("default"));
+    public BookServiceBlockingStub bookServiceBlockingStub() {
+        var channel = ManagedChannelBuilder
+                .forAddress("127.0.0.1", 8080)
+                .usePlaintext()
+                .build();
+        return BookServiceGrpc.newBlockingStub(channel);
     }
 }
